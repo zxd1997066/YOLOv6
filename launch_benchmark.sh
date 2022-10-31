970005952ddd8d4e2ab1c183196bd7bad2112a78
mengfeil <mengfei.li@intel.com>
2022-10-31 00:25:41 -0700

    add cuda launch

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -38,7 +38,7 @@ function main {
             # clean workspace
             logs_path_clean
             # generate launch script for multiple instance
-            if [ "${OOB_USE_LAUNCHER}" != "1" ];then
+            if [ "${OOB_USE_LAUNCHER}" != "1" ] && [ "${device}" != "cuda" ];then
                 generate_core
             else
                 generate_core_launcher
@@ -57,14 +57,19 @@ function main {
 
 # run
 function generate_core {
+    if [ "${device}" != "cuda" ];then
+        OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+        OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+    else
+        OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+    fi
     # generate multiple instance script
     for(( i=0; i<instance; i++ ))
     do
         real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
         log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
 
-        printf " numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') \
-                    -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') \
+        printf " ${OOB_EXEC_HEADER} \
             python tools/infer.py --weights $CKPT_DIR \
                 --source $DATASET_DIR \
                 --num_iter $num_iter --num_warmup $num_warmup \
